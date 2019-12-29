a72e9ae3bd14d5cb7b78c841b8e28ba365d6d545
sanjiv sahayam <sanjsmailbox@gmail.com>
2019-12-29 23:53:12 +1100

    Generalise

--- src/Blog/UnfoldExample.hs
+++ src/Blog/UnfoldExample.hs
@@ -1,10 +1,9 @@
 {-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE KindSignatures   #-}
 
 module Blog.UnfoldExample (
-    getData
-  , process
-  , processPacket
+    serviceCall
+  , unfoldWith
+  , extractState
   , run
   , Cursor(..)
   , CursorState(..)
@@ -15,36 +14,50 @@ import Data.List (intercalate)
 import Control.Monad.Loops (unfoldrM)
 import Data.Kind (Type)
 
-newtype Cursor = Cursor Int deriving Eq
-data CursorState = NewCursor | GoCursor Cursor | StopCursor
+-- | Wrapper around a cursor of some sort
+newtype Cursor a = Cursor a deriving Eq
 
-data Packet = Packet { value :: String, cursor :: Maybe Cursor }
+-- | An ADT to capture the three states of a cursor:
+-- NewCursor - Initial cursor state
+-- GoCursor - A state of having a cursor, typically used for iteration
+-- StopCursor - The final cursor state
+data CursorState a = NewCursor | GoCursor (Cursor a) | StopCursor
 
-getData :: forall (m :: Type -> Type). Applicative m => Maybe Cursor -> m Packet
-getData Nothing = pure $ Packet "packet one" (Just $ Cursor 1)
-getData (Just (Cursor cur))
+-- | A Generic function that unfolds a computation that has some form of 'Cursor'; such as a paginated REST API.
+unfoldWith :: forall m a b c. Applicative m => 
+  (a -> (b, CursorState c)) -> 
+  -- ^ a function that can extract a "result" and the next 'CursorState' from some response 'a'
+  (Maybe (Cursor c) -> m a)   -> 
+  -- ^ a function that takes an optional 'Cursor' and returns a response 'a' in the effect 'm'
+  (CursorState c)            -> 
+  -- ^ an initial 'CursorState'
+  m (Maybe (b, CursorState c))
+  -- ^ the optional pair of the result type 'b' with the next 'CursorState' in the effect 'm'
+unfoldWith extractPayload iterateWith NewCursor = 
+  let resultM :: m a = iterateWith Nothing
+  in  Just . extractPayload <$> resultM
+unfoldWith extractPayload iterateWith (GoCursor c) = 
+  let resultM :: m a = iterateWith (Just c)
+  in  Just . extractPayload <$> resultM
+unfoldWith _ _ StopCursor = pure Nothing 
+
+data Packet = Packet { value :: String, cursor :: Maybe (Cursor Int) }
+
+serviceCall :: forall m . Applicative m => Maybe (Cursor Int) -> m Packet
+serviceCall Nothing = pure $ Packet "packet one" (Just $ Cursor 1)
+serviceCall (Just (Cursor cur))
   | cur == 1  = pure $ Packet "packet two"   (Just $ Cursor 2)
   | cur == 2  = pure $ Packet "packet three" (Just $ Cursor 3)
   | cur == 3  = pure $ Packet "packet four"  (Just $ Cursor 4)
   | otherwise = pure $ Packet "packet five"  Nothing
 
-process :: forall (m :: Type -> Type). Applicative m => (Maybe Cursor -> m Packet) -> CursorState -> m (Maybe (String, CursorState))
-process f NewCursor = 
-  let resultM :: m Packet = f Nothing
-  in  Just . processPacket <$> resultM
-process f (GoCursor c) = 
-  let resultM :: m Packet = f (Just c)
-  in  Just . processPacket <$> resultM
-process _ StopCursor = pure Nothing 
-
-
-processPacket :: Packet -> (String, CursorState)
-processPacket (Packet v (Just c)) = (v, GoCursor c)
-processPacket (Packet v Nothing)  = (v, StopCursor)
+extractState :: Packet -> (String, CursorState Int)
+extractState (Packet v (Just c)) = (v, GoCursor c)
+extractState (Packet v Nothing)  = (v, StopCursor)
 -- unfoldrM :: Monad m => (a -> m (Maybe (b, a))) -> a -> m [b]
 
 run :: IO ()
 run = 
-  let resultsIO :: IO [String] = unfoldrM (process getData) NewCursor
+  let resultsIO :: IO [String] = unfoldrM (unfoldWith extractState serviceCall) NewCursor
       stringyfied :: IO String = (intercalate "," . take 3) <$> resultsIO
   in stringyfied >>= putStrLn
